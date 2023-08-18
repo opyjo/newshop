322f544bb666030aadd4fff09849e3d4f32ec333
Johnson Ojo <johnson.ojo@bell.ca>
2023-08-18 09:07:18 -0400

    Setting up the redux tool kit

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,20 +1,39 @@
 import { Row, Col } from "react-bootstrap";
-import products from "../products";
 import Product from "../components/Product";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { useGetProductsQuery } from "../slices/ProductsApiSlice";
 
 const HomeScreen = () => {
-  return (
-    <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
-    </>
-  );
+  const { data: products, isLoading, error } = useGetProductsQuery();
+
+  // A function to handle rendering based on the state
+  const renderContent = () => {
+    if (isLoading) {
+      return <Loader />;
+    }
+    if (error) {
+      return (
+        <Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>
+      );
+    }
+    return (
+      <>
+        <h1>Latest Products</h1>
+        <Row>
+          {products.map((product) => (
+            <Col sm={12} md={6} lg={4} xl={3} key={product._id}>
+              <Product product={product} />
+            </Col>
+          ))}
+        </Row>
+      </>
+    );
+  };
+
+  return <>{renderContent()}</>;
 };
 
 export default HomeScreen;
